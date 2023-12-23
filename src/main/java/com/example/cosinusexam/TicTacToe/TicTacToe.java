efba72ddf8ee79e645665173922a2a0f2d13a9c8
UzCoder <kimsanov0717@gmail.com>
2023-12-23 13:18:48 +0500

    Initial commit

---
+++ src/main/java/com/example/cosinusexam/TicTacToe/TicTacToe.java
@@ -0,0 +1,2 @@
+package com.example.cosinusexam.TicTacToe;public class TicTacToe {
+}
---
+++ src/test/java/com/example/cosinusexam/CosinusExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cosinusexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosinusExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
